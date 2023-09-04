cc857c9c5039f330a2e4d4c690944aaf2d9fc7cb
Erick Calegaro <erick.calegaro@gertec.com.br>
2023-09-04 20:30:40 -0300

    Implementado esqueleto da tela de username

---
+++ front/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import io from 'socket.io-client'
+import {useState} from 'react'
+
+const socket = io.connect("http//localhost:3001")
+
+function App() {
+  const [username, setUsername] = useState("")
+  const [channel , setChannel ] = useState("")
+
+  const joinChannel = () => {
+    if(username !== "" && channel !== ""){
+      socket.emit("join_channel", channel)
+    }
+  };
+
+  return (
+    <div className="App">
+ 
+      <h3>ChatSync</h3>
+      <input type="text" placeholder="Apelido..." onChange = {
+        (event) => {
+          setUsername(event.target.value)
+        }
+      }/>
+      <input type="text" placeholder="ID do canal..." onChange = {
+        (event) => {
+          setChannel(event.target.value)
+        }
+      }/>
+      <button> Entrar no canal</button>
+    </div>
+  );
+}
+
+export default App;
