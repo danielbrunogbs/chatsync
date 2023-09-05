ef415edd02cbe3937349015033ad0c5044a6dae0
Erick Calegaro <erick.calegaro@gertec.com.br>
2023-09-04 21:02:21 -0300

    Alterada porta de conexão para 4000

--- front/src/App.js
+++ front/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import io from 'socket.io-client'
 import {useState} from 'react'
 
-const socket = io.connect("http//localhost:3001")
+const socket = io.connect("localhost:4000")
 
 function App() {
   const [username, setUsername] = useState("")
