db6b90c04966d2c7bb8811e7756f3b35420b3543
Erick Calegaro <erick.calegaro@gertec.com.br>
2023-09-06 16:14:23 -0300

    Implementado esboço de avatar

--- front/src/App.js
+++ front/src/App.js
@@ -23,7 +23,25 @@ function App() {
           setUsername(event.target.value)
         }
       }/>
+      <text> </text>
       <button onClick={joinChannel}>Entrar no canal</button>
+      <br/>
+      <br/>
+      <h2>Escolha seu avatar</h2>
+      <div class="container">
+        <label class="rad">
+        <input type="radio" name="rad1" value="a"/>
+        <i/><img class="avatar" src="https://a.imagem.app/bOkpZ1.png" alt="Option 1"/>
+        </label>
+        <label class="rad">
+          <input type="radio" name="rad1" value="b" checked/>
+          <i/><img class="avatar" src="https://a.imagem.app/bOzA8W.png" alt="Option 2"/>
+        </label>
+        <label class="rad">
+          <input type="radio" name="rad1" value="c" checked/>
+          <i/><img class="avatar" src="https://a.imagem.app/bOkWxQ.png" alt="Option 3"/>
+        </label>
+      </div>
     </div>
   );
 }
