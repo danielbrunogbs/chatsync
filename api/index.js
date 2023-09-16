d32293f8acc1d6a4f4771ad6d1602064e1043fb9
Daniel Bruno <danielbrunogbs@gmail.com>
2023-09-16 00:09:09 -0300

    Implemented message functions.

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import { Server } from "socket.io"
 import cors from "cors"
 import http from "http"
 import JoinChannelController from "./app/controllers/JoinChannelController.js"
+import SendMessageController from "./app/controllers/SendMessageController.js"
 
 dotenv.config();
 
@@ -25,6 +26,7 @@ server.on("connection", socket => {
     console.log(`Connection received: ${socket.id}`);
 
     socket.on("join_channel", (username, sex) => JoinChannelController(socket, username, sex));
+    socket.on("send_message", (data) => SendMessageController(socket, data));
 
     socket.on("disconnect", () => console.log(`Disconnect: ${socket.id}`));
 
